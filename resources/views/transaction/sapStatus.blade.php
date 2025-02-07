492fbe482b01905605cd071429a46d3b70aaed48
Mahesa <baskorocr@gmail.com>
2025-02-07 08:58:19 +0700

    little change

--- resources/views/transaction/sapStatus.blade.php
+++ resources/views/transaction/sapStatus.blade.php
@@ -10,7 +10,7 @@
         <div class="flex flex-col md:flex-row justify-between items-center gap-4 mb-4">
             <!-- Search & Date Filters (Left) -->
             <div class="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-                <input type="text" id="searchInput" value="{{ $search }}" placeholder="Search by name"
+                <input type="text" id="searchInput" value="{{ $search }}" placeholder="Search by no order"
                     class="w-full md:w-64 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600">
 
                 <div class="flex items-center gap-2 w-full md:w-auto">
